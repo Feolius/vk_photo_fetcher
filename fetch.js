29c986cf65d4bc04e761cf5a53312a184b114de0
feolius <feolius@gmail.com>
2017-08-28 19:49:50 +0700

    Rethought auth logic a little. Added filters by photo id

--- fetch.js
+++ fetch.js
@@ -1,4 +1,5 @@
 "use strict";
+const VK_ACCESS_TOKEN_STORAGE_KEY = 'pf_vkaccess_token';
 $(function() {
     let currentUrl = window.location.href;
     let urlParser = document.createElement('a');
@@ -11,29 +12,63 @@ $(function() {
             messageId = paramKeyValue[1];
         }
     }
-    chrome.runtime.sendMessage({action: "fetchPhotoAttachments", messageId: messageId}, function (response) {
-        if(response.error !== undefined) {
-            console.log(response.error);
-        } else if(response.result !== undefined) {
-            let container = $(".container");
+    let container = $(".container");
+    chrome.storage.local.get({[VK_ACCESS_TOKEN_STORAGE_KEY]: {}}, function (items) {
+        if (items[VK_ACCESS_TOKEN_STORAGE_KEY].length === undefined) {
             container.empty();
-            container.append('<select multiple="multiple" class="photo-select image-picker masonry">');
-            let select = $(".photo-select");
-            for (let i = 0; i < response.result.length; i++) {
-                let photo = response.result[i].attachment.photo;
-                select.append('<option data-img-src="' + photo.photo_604 + '" value="' + photo.id +'">Option' + i + '</option>')
-            }
-            select.imagepicker();
-            let pickerContainer = select.next("ul.thumbnails");
-            pickerContainer.imagesLoaded(function(){
-                pickerContainer.masonry({
-                    itemSelector:   "li",
+            let errorContainer = $('<div class="errors-container bg-danger"></div>');
+            container.append(errorContainer);
+            container.append('<div class="text-primary">Authorization needed. Click to authorize in VK.</div>');
+            let authBtn = $('<button type="button" class="btn btn-primary vk-auth-btn">VK auth</button>');
+            container.append(authBtn);
+            authBtn.click(function () {
+                chrome.runtime.sendMessage({action: "auth"}, function (response) {
+                    if(response.error !== undefined) {
+                        errorContainer.html(response.error);
+                    } else {
+                        location.reload(true);
+                    }
                 });
             });
+        } else {
+            chrome.runtime.sendMessage({action: "fetchPhotoAttachments", messageId: messageId}, function (response) {
+                if(response.error !== undefined) {
+                    if(response.error.error_code !== undefined && response.error.error_code === 5) {
+                        chrome.storage.local.remove(VK_ACCESS_TOKEN_STORAGE_KEY, function () {
+                            location.reload(true);
+                        });
+                    } else {
+                        container.empty();
+                        container.append('<div class="bg-danger">Photo fetch error: ' + response.error + '</div>');
+                    }
+                } else if(response.result !== undefined) {
+                    let items = response.result.items;
+                    container.empty();
+                    container.append('<select multiple="multiple" class="photo-select image-picker masonry">');
+                    let select = $(".photo-select");
+                    let photos = [];
+                    // Get rid of duplicates here using photo id.
+                    for (let i = 0; i < items.length; i++) {
+                        let photo = items[i].attachment.photo;
+                        photos[photo.id] = photo;
+                    }
+                    for (let id in photos) {
+                        let photo = photos[id];
+                        select.append('<option data-img-src="' + photo.photo_604 + '" value="' + id +'">Option' + id + '</option>');
+                    }
+                    select.imagepicker();
+                    let pickerContainer = select.next("ul.thumbnails");
+                    pickerContainer.imagesLoaded(function(){
+                        pickerContainer.masonry({
+                            itemSelector:   "li",
+                        });
+                    });
 
-            console.log(response.result);
+                    console.log(response.result);
+                }
+
+            });
         }
-
     });
-    console.log(messageId);
+
 });
--- popup.js
+++ popup.js
@@ -1,51 +1,33 @@
 "use strict";
-const VK_ACCESS_TOKEN_STORAGE_KEY = 'pf_vkaccess_token';
-chrome.storage.local.get({[VK_ACCESS_TOKEN_STORAGE_KEY]: {}}, function (items) {
-    if (items[VK_ACCESS_TOKEN_STORAGE_KEY].length === undefined) {
-        // We need to insert button for VK auth.
-        let authBtnWrapper = document.createElement("div");
-        let authBtn = document.createElement("button");
-        let authBtnText = document.createTextNode("Auth");
-        authBtn.appendChild(authBtnText);
-        authBtnWrapper.appendChild(authBtn);
-        document.body.appendChild(authBtnWrapper);
-        authBtn.addEventListener("click", function () {
-            chrome.runtime.sendMessage({action: "auth"});
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    let url = tabs[0].url;
+    let urlParser = document.createElement('a');
+    urlParser.href = url;
+    let messageId = "";
+    if (urlParser.hostname === "vk.com") {
+        // Remove ? symbol.
+        let paramsKeysValues = urlParser.search.substring(1).split("&");
+        for (let i = 0; i < paramsKeysValues.length; i++) {
+            let paramKeyValue = paramsKeysValues[i].split("=");
+            if (paramKeyValue[0] === "sel")
+                messageId = paramKeyValue[1];
+        }
+    }
+    if(messageId !== "") {
+        let fetchBtnWrapper = document.createElement("div");
+        let fetchBtn = document.createElement("button");
+        let fetchBtnText = document.createTextNode("Fetch");
+        fetchBtn.appendChild(fetchBtnText);
+        fetchBtnWrapper.appendChild(fetchBtn);
+        document.body.appendChild(fetchBtnWrapper);
+        fetchBtn.addEventListener("click", function () {
+            chrome.tabs.create({url: "fetch.html?messageId=" + messageId, selected: true}, function (tab) {
+
+            });
+
         });
     } else {
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-            let url = tabs[0].url;
-            let urlParser = document.createElement('a');
-            urlParser.href = url;
-            let messageId = "";
-            if (urlParser.hostname === "vk.com") {
-                // Remove ? symbol.
-                let paramsKeysValues = urlParser.search.substring(1).split("&");
-                for (let i = 0; i < paramsKeysValues.length; i++) {
-                    let paramKeyValue = paramsKeysValues[i].split("=");
-                    if (paramKeyValue[0] === "sel")
-                        messageId = paramKeyValue[1];
-                }
-            }
-            if(messageId !== "") {
-                let fetchBtnWrapper = document.createElement("div");
-                let fetchBtn = document.createElement("button");
-                let fetchBtnText = document.createTextNode("Fetch");
-                fetchBtn.appendChild(fetchBtnText);
-                fetchBtnWrapper.appendChild(fetchBtn);
-                document.body.appendChild(fetchBtnWrapper);
-                fetchBtn.addEventListener("click", function () {
-                    chrome.tabs.create({url: "fetch.html?messageId=" + messageId, selected: true}, function (tab) {
-
-                    });
-
-                });
-            } else {
-                showErrors(["In order to fetch photos you need to be on a message page."])
-            }
-
-
-        });
+        showErrors(["In order to fetch photos you need to be on a message page."])
     }
 });
 
