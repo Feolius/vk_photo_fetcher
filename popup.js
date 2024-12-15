be9604f4ec79d6e8f0b6786a9b7abeac66af978d
IvanStrygin <feolius@gmail.com>
2024-12-15 21:12:25 +0300

    Bring back old way of getting chat id.

--- popup.js
+++ popup.js
@@ -1,15 +1,11 @@
 (function () {
     "use strict";
     chrome.tabs.query({"active": true, "lastFocusedWindow": true}, (tabs) => {
-        const urlParser = new URL(tabs[0].url);
-        // We are checking for path urls to match the pattern vk.com/im/convo/{xxxx} where {xxxx} is message id.
-        const pathParts = urlParser.pathname.split("/");
-        if (urlParser.hostname !== "vk.com" || pathParts.length !== 4 ||
-            pathParts[0] !== "" || pathParts[1] !== "im" || pathParts[2] !== "convo") {
+        const chatId = extractChatId(tabs[0].url);
+        if (!chatId) {
             displayErrors([chrome.i18n.getMessage("outOfDialogPageMsg")]);
             return;
         }
-        const messageId = pathParts[3];
 
         const fetchBtnWrapper = document.createElement("div");
         const fetchBtn = document.createElement("button");
@@ -22,9 +18,38 @@
         container[0].appendChild(fetchBtnWrapper);
         document.body.appendChild(fetchBtnWrapper);
         fetchBtn.addEventListener("click", () => {
-            chrome.tabs.create({url: `fetch.html?messageId=${messageId}`, selected: true}, () => {});
+            chrome.tabs.create({url: `fetch.html?messageId=${chatId}`, selected: true}, () => {});
         });
     });
+
+    /**
+     * @param {string} url
+     * @return {string|null}
+     */
+    const extractChatId = (url) => {
+        const urlParser = new URL(url);
+        if (urlParser.hostname !== "vk.com") {
+            return null;
+        }
+        const pathParts = urlParser.pathname.split("/");
+        // Checking for path urls to match the pattern vk.com/im/convo/{xxxx} where {xxxx} is message id.
+        if (pathParts.length === 4 && pathParts.slice(0, 3).join("/") === "/im/convo") {
+            return pathParts[3];
+        }
+        // Checking for old format vk.com/im?sel={xxxx}, where {xxxx} can be group id starting from "c" or numeric id.
+        if (pathParts.length === 2 && urlParser.pathname === "/im") {
+            const selParam = urlParser.searchParams.get("sel");
+            if (selParam.charAt(0) === "c") {
+                const selNumber = Number.parseInt(selParam.slice(1))
+                if (!isNaN(selNumber)) {
+                    return (2000000000 + selNumber).toString()
+                }
+            } else {
+                return selParam;
+            }
+        }
+        return null;
+    }
 
     function displayErrors(errors) {
         const errorsContainer = document.getElementById("errors-container");
